3bd2e0fd3f2fab174f3fc72bd59909dec9fb1f7b
Rafael White <rwhite@mctekk.com>
2020-04-01 16:29:47 -0400

    Revert changes autoloader

--- library/Core/autoload.php
+++ library/Core/autoload.php
@@ -1,30 +1,12 @@
 <?php
 
 use Dotenv\Dotenv;
-use Phalcon\Loader;
 use function Canvas\Core\appPath;
 
-$loader = new Loader();
-$namespaces = [
-    'Canvas' => '/canvas-core/src',
-    'Canvas\Cli' => '/canvas-core/src/Cli',
-    'Baka\Auth' => '/baka/auth/src',
-    'Baka\Database' => '/baka/database/src',
-    'Baka\Elasticsearch' => '/baka/elasticsearch/src',
-    'Baka\Http' => '/baka/http/src',
-    'Phalcon\Cashier' => '/baka/cashier/src',
-    'Baka\Mail' => '/baka/mail/src',
-    'Baka\Blameable' => '/baka/blameable/src',
-    'Baka\Support' => '/baka/support/src',
-    'Baka\Router' => '/baka/router/src'
-];
+// Register the auto loader
+require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . '/vendor/canvas/core/src/Core/functions.php';
 
-$loader->registerNamespaces($namespaces);
-
-$loader->register();
-/**
- * Composer Autoloader.
- */
+// Composer Autoloader.
 require appPath('vendor/autoload.php');
 
 // Load environment
