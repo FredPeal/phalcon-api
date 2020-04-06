4ee91cdc8049dd5dc2da47354435ca06dd79e95e
Rafael White <rwhite@mctekk.com>
2020-04-06 09:59:15 -0400

    Add die to see error

--- tests/api/RolesCrudAccessListCest.php
+++ tests/api/RolesCrudAccessListCest.php
@@ -86,6 +86,9 @@ class RolesCrudAccessListCest extends BakaRestTest
         $response = $I->grabResponse();
         $data = json_decode($response, true);
 
+        print_r($data);
+        die();
+
         $I->sendPUT('/v1/' . $this->model . '/' . $data[count($data) - 1]['roles_id'], $role);
 
         $I->seeResponseIsSuccessful();
