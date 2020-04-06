d6ff40e6a260df4da6f44201de020fc093558e5f
Rafael White <rwhite@mctekk.com>
2020-04-06 12:23:11 -0400

    Add cli task for testing error

---
+++ cli/tasks/SetupTask.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Gewaer\Cli\Tasks;
+
+use EmailTemplates;
+use Phalcon\Cli\Task as PhTask;
+use Gewaer\Cli\Tasks\AclTask;
+use Gewaer\Cli\Tasks\EmailtemplatesTask;
+
+/**
+ * Class AclTask.
+ *
+ * @package Canvas\Cli\Tasks;
+ *
+ * @property \Canvas\Acl\Manager $acl
+ */
+class SetupTask extends PhTask
+{
+    /**
+     * Create the default roles of the system.
+     *
+     * @return void
+     */
+    public function mainAction()
+    {
+        $acl = new AclTask();
+        $emailTemplates = new EmailtemplatesTask();
+
+        $acl->setupDefaultRoles();
+        $acl->kanvas();
+        $emailTemplates->insertUserNotificationTemplate();
+    }
+}
